017ac789f164090019e730f205c23f55eabf3f99
Anders Fredrik Kiær <akia@statoil.com>
2019-06-14 15:55:09 +0200

    Change base image

---
+++ gunicorn_conf.py
@@ -0,0 +1,13 @@
+import multiprocessing
+
+# For documentation on gunicorn configuration settings, see
+# http://docs.gunicorn.org/en/stable/settings.html
+
+worker_class='egg:meinheld#gunicorn_worker'
+workers = 2 * multiprocessing.cpu_count()
+
+bind = '0.0.0.0:5000'
+keepalive = 120
+
+loglevel = 'info'
+errorlog = '-'
