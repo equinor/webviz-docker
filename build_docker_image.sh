f47f876d602c4c08e03a92819594f86a41c40785
Anders Fredrik Kiær <akia@statoil.com>
2019-06-24 13:31:51 +0200

    Change from backticks  to

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PIP_PACKAGES=`echo $(<./requirements.txt) | tr "\n" " "`
+PIP_PACKAGES=$(echo $(<./requirements.txt) | tr "\n" " ")
 
 docker build -t webviz/base_image . --build-arg PIP_PACKAGES="$PIP_PACKAGES"
