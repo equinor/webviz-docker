03f5ca4fea20e27a686f0caea52705056a212d6a
Anders Fredrik Kiær <akia@statoil.com>
2019-06-24 13:25:33 +0200

    Add "" around Docker build-arg

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -2,4 +2,4 @@
 
 PIP_PACKAGES=`echo $(<./requirements.txt) | tr "\n" " "`
 
-docker build -t webviz/base_image . --build-arg PIP_PACKAGES=$PIP_PACKAGES
+docker build -t webviz/base_image . --build-arg PIP_PACKAGES="$PIP_PACKAGES"
