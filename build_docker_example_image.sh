c16362f636a1e9b7efb77cb3b724ef4b477e6ab6
Anders Fredrik Kiær <akia@statoil.com>
2019-09-03 10:41:13 +0200

    Checkout installed webviz-config example

--- build_docker_example_image.sh
+++ build_docker_example_image.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+WEBVIZ_CONFIG_VERSION=`python -c "import webviz_config; print(webviz_config.__version__)"`
+
 git clone https://github.com/equinor/webviz-config.git
+
+pushd webviz-config
+git checkout $WEBVIZ_CONFIG_VERSION
+
 webviz certificate
 webviz build webviz-config/examples/basic_example.yaml --portable ./example_app
+
 pushd example_app
 docker build -t webviz/example_image
+popd
+
 popd
