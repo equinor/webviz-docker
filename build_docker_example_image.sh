cffb77a8791cd238e98c630a175362bcbd274d1f
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2019-09-03 15:31:34 +0200

    Fix example image build (#16)

--- build_docker_example_image.sh
+++ build_docker_example_image.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+set -e
+
 WEBVIZ_CONFIG_VERSION=$(python -c "import webviz_config; print(webviz_config.__version__)")
 
 git clone https://github.com/equinor/webviz-config.git
 
 pushd webviz-config
 git checkout $WEBVIZ_CONFIG_VERSION
+popd
 
 webviz certificate
 webviz build webviz-config/examples/basic_example.yaml --portable ./example_app
 
 pushd example_app
-docker build -t webviz/example_image
-popd
-
+docker build -t webviz/example_image .
 popd
--- deploy_docker_images.sh
+++ deploy_docker_images.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push webviz/base_image:latest
