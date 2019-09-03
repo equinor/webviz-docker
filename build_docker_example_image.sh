0e1f3b5361b10899f2e338ae3721ba98b88d0618
Anders Fredrik Kiær <akia@statoil.com>
2019-09-03 12:43:39 +0200

    Move popd

--- build_docker_example_image.sh
+++ build_docker_example_image.sh
@@ -8,12 +8,11 @@ git clone https://github.com/equinor/webviz-config.git
 
 pushd webviz-config
 git checkout $WEBVIZ_CONFIG_VERSION
+popd
 
 webviz certificate
 webviz build webviz-config/examples/basic_example.yaml --portable ./example_app
 
 pushd example_app
 docker build -t webviz/example_image
-popd
-
 popd
